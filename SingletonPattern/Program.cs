110ac7a70a9b66008408680c2c7910e22573f374
Мария Юдина <mahayudina@mail.ru>
2017-12-29 21:59:15 +0300

    Добавьте файлы проекта.

---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton single = Singleton.GetInstance();
+            single.test = 1;
+            Console.WriteLine(single.test);
+
+            Singleton single2 = Singleton.GetInstance();
+            single2.test = 4;
+            Console.WriteLine(single.test);
+
+            Console.ReadKey();
+        }
+    }
+}
